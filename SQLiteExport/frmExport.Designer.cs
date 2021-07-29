c07fea9b1bc41d42ec80108ec13b7db1d5373b5d
zhouxtan <zhouxtan@cn.ibm.com>
2021-07-29 16:56:26 +0800

    init

---
+++ SQLiteExport/frmExport.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SQLiteExport
+{
+    partial class frmExport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSQLite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSQLite
+            // 
+            this.txtSQLite.Location = new System.Drawing.Point(12, 34);
+            this.txtSQLite.Name = "txtSQLite";
+            this.txtSQLite.Size = new System.Drawing.Size(454, 20);
+            this.txtSQLite.TabIndex = 1;
+            this.txtSQLite.Text = "C:\\Workspace\\Fukui\\SoureCode\\_MstMaintUpgrade\\MstMaint\\MstMaint.sdb";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SQLite File";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 108);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 221);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "MENUITEM\r\nCOLINFO\r\nFILTER\r\nPICKER\r\nSETTINGS\r\nACCLIST\r\nACCGROUP";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(12, 80);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(454, 20);
+            this.txtFolder.TabIndex = 4;
+            this.txtFolder.Text = "C:\\Workspace\\Fukui\\data\\新しいフォルダー";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Save Folder";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(472, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(472, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 20);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 341);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSQLite);
+            this.Controls.Add(this.button1);
+            this.Name = "frmExport";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSQLite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SQLiteExport/frmExport.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SQLiteExport
+{
+    public partial class frmExport : Form
+    {
+        string cs = @"URI=file:C:\Workspace\Fukui\SoureCode\_MstMaintUpgrade\MstMaint\MstMaint.sdb";
+
+        public frmExport()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtSQLite.Text)) {
+                MessageBox.Show("Please select SQLite File!!!");
+                return ;
+            }
+            if(string.IsNullOrEmpty(txtFolder.Text)) {
+                MessageBox.Show("Please select output folder!!!");
+                return ;
+            }
+            cs = @"URI=file:" + txtSQLite.Text;
+            var list = textBox2.Lines;
+            foreach (var item in list)
+            {
+                try
+                {
+                    export_Data(item);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(item + " export Fail!!!");
+                }
+            }
+            MessageBox.Show("Export Completed!!!!!");
+        }
+
+        private void export_Data(string tableName)
+        {
+            var con = new System.Data.SQLite.SQLiteConnection(cs);
+            con.Open();
+            string sql = " select * from " + tableName;
+            var dta = new System.Data.SQLite.SQLiteDataAdapter(sql, con);
+            var ds = new DataSet();
+            dta.Fill(ds);
+            con.Close();
+
+            List<string> output = new List<string>();
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                string outrow = string.Empty;
+                foreach (DataColumn item in ds.Tables[0].Columns)
+                {
+                    var data = string.Empty;
+                    if (row[item] != null)
+                    {
+                        data = row[item].ToString();
+                        if (data.Length > 100) data = "";
+                    }
+                    if (item.DataType.ToString() == "System.String")
+                    {
+                        if (data.Contains("\r") || data.Contains("\n"))
+                        {
+                            data = data.Replace("\r", "\\r").Replace("\n", "\\n");
+                        }
+                        if (data.Contains("\""))
+                        {
+                            data = data.Replace("\"", "\"\"");
+                        }
+                        if (item.ColumnName == "LAST_OP_ID")
+                        {
+                            if (data == string.Empty) data = "CICDL";
+                        }
+                        if (data == string.Empty) data = " ";
+                        data = "\"" + data + "\"";
+                        outrow += data + ",";
+                    }
+                    else if (item.DataType.ToString() == "System.Int32")
+                    {
+                        if (data == string.Empty) data = "0";
+                        outrow += data + ",";
+                    }
+                    else if (item.DataType.ToString() == "System.DateTime")
+                    {
+                        if (item.ColumnName == "LAST_DATE")
+                        {
+                            if (data == string.Empty)
+                            {
+                                data = "20210714";
+                            }
+                            else
+                            {
+                                data = DateTime.Parse(row[item].ToString()).ToString("yyyyMMdd");
+                            }
+                        }
+                        else if (item.ColumnName == "LAST_TIME")
+                        {
+                            if (data == string.Empty)
+                            {
+                                data = "00.00.01";
+                            }
+                            else
+                            {
+                                data = DateTime.Parse(row[item].ToString()).ToString("HH.mm.ss");
+                            }
+
+                        }
+                        outrow += data + ",";
+                    }
+                }
+                if (outrow[outrow.Length - 1] == ',')
+                {
+                    outrow = outrow.Remove(outrow.Length - 1);
+                }
+                output.Add(outrow);
+                //outrow += Environment.NewLine;
+
+            }
+            if (output.Count > 0)
+            {
+                var filename = txtFolder.Text + "\\" + tableName + ".del";
+                File.WriteAllLines(filename, output, Encoding.GetEncoding("Shift-JIS"));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dia = new OpenFileDialog();
+            dia.DefaultExt = "*.*|*.*";
+            dia.Multiselect = false;
+            if (txtSQLite.Text != string.Empty)
+            {
+                dia.FileName = txtSQLite.Text;
+            }
+            if (dia.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSQLite.Text = dia.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dlg = new FileFolderDialog();
+            dlg.IsDialog = true;
+            if (txtFolder.Text != string.Empty)
+            {
+                dlg.SelectedPath = txtFolder.Text;
+            }
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = dlg.SelectedPath;
+            }
+        }
+    }
+}
